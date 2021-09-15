8848c18f6e899424e8a0b5b8f5df6b6224b6d639
dominggo1999 <arnolddominggo19@gmail.com>
2021-09-16 00:41:36 +0700

    Scroll To Top

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 import Routes from './Routes';
 
 const App = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    // Scroll to top when detecting route change
+    window.scrollTo(0, 0);
+  }, [location]);
+
   return (
     <>
       <Navbar />
